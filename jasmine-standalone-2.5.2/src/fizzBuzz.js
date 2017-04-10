bfc5cc9321ea17656348ae0b1b4e17259fd4a512
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-04-10 18:05:08 +0100

    adds remainder of tests and src for FizzBuzz

--- jasmine-standalone-2.5.2/src/fizzBuzz.js
+++ jasmine-standalone-2.5.2/src/fizzBuzz.js
@@ -2,10 +2,24 @@ function FizzBuzz() {
 }
 
 FizzBuzz.prototype.play = function(number) {
-  if (number % 3 == 0) {
+
+  if (number % 5 == 0 && number % 3 == 0) {
+    return "FizzBuzz";
+  }
+    else if (number % 3 == 0) {
     return "Fizz";
+  }
+  else if (number % 5 == 0 ) {
+    return "Buzz";
   }
   else {
     return number;
   }
 }
+
+// to print the numbers, run this in the Dev Console:
+// var FizzBuzz = new FizzBuzz();
+
+// for (var i = 1; i <= 100; i++) {
+//   console.log(fizzBuzz.play(i));
+// }
