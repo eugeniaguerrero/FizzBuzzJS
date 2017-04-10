3314bb46e40f2f6612bcdaa9710aa728097148c4
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-04-10 17:22:26 +0100

    initial commit: adds spec and src files

---
+++ jasmine-standalone-2.5.2/spec/fizzBuzzSpec.js
@@ -0,0 +1,5 @@
+describe("Fizz", function() {
+  it("returns 'Fizz' for multiples of 3", function() {
+    expect(FizzBuzz.prototype.play(3)).toEqual('Fizz');
+  });
+});
---
+++ jasmine-standalone-2.5.2/src/fizzBuzz.js
@@ -0,0 +1,11 @@
+function FizzBuzz() {
+}
+
+FizzBuzz.prototype.play = function(number) {
+  if (number % 3 == 0) {
+    return "Fizz";
+  }
+  else {
+    return number;
+  }
+}
